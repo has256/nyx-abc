98229c219579a50b40d8f8d4214db5de3478e5bc
Carlos EA. Batista <carlos.batista@eiprice.com.br>
2021-03-03 19:35:34 -0300

    :sparkles: we have sites

--- cmd/site.go
+++ cmd/site.go
@@ -1,22 +1,61 @@
 package cmd
 
 import (
-	"fmt"
+	"log"
 	"os"
+	"os/exec"
 
 	"github.com/spf13/cobra"
 )
 
+func openBrowser(url string) {
+	var err error
+	exec.Command("xdg-open", url).Start()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
 var (
 	tidia   bool
+	prograd bool
+	gradmat bool
+	bcc     bool
+	sigaa   bool
+	moodle  bool
+	neuro   bool
+	ufabc   bool
 	siteCmd = &cobra.Command{
 		Use:   "site",
 		Short: "Abre os horrorosos sites da UFABC",
 		Run: func(cmd *cobra.Command, args []string) {
+			// refact, do we have PM? Switch/case?
 			if tidia {
-				fmt.Println("site called")
+				openBrowser("https://tidia4.ufabc.edu.br/portal/relogin")
+				os.Exit(0)
+			} else if prograd {
+				openBrowser("https://prograd.ufabc.edu.br/")
+				os.Exit(0)
+			} else if gradmat {
+				openBrowser("https://gradmat.ufabc.edu.br/")
+				os.Exit(0)
+			} else if bcc {
+				openBrowser("https://bcc.ufabc.edu.br")
+				os.Exit(0)
+			} else if sigaa {
+				openBrowser("https://sig.ufabc.edu.br/sigaa/verTelaLogin.do")
+				os.Exit(0)
+			} else if moodle {
+				openBrowser("https://moodle.ufabc.edu.br/")
+				os.Exit(0)
+			} else if neuro {
+				openBrowser("https://neuro.ufabc.edu.br/")
+				os.Exit(0)
+			} else if ufabc {
+				openBrowser("https://www.ufabc.edu.br/")
 				os.Exit(0)
 			}
+
 			if len(args) == 0 {
 				cmd.Help()
 			}
@@ -29,6 +68,41 @@ func init() {
 	siteCmd.Flags().BoolVarP(
 		&tidia, "tidia",
 		"t", false, "abre o site do tidia",
+	)
+
+	siteCmd.Flags().BoolVarP(
+		&prograd, "prograd",
+		"p", false, "abre o site da prograd",
+	)
+
+	siteCmd.Flags().BoolVarP(
+		&gradmat, "gradmat",
+		"g", false, "abre o site do gradmat",
+	)
+
+	siteCmd.Flags().BoolVarP(
+		&sigaa, "sigaa",
+		"s", false, "abre o site do sigaa",
+	)
+
+	siteCmd.Flags().BoolVarP(
+		&moodle, "moodle",
+		"m", false, "abre o site do moodle",
+	)
+
+	siteCmd.Flags().BoolVarP(
+		&ufabc, "ufabc",
+		"u", false, "abre o site da ufabc",
+	)
+
+	siteCmd.Flags().BoolVarP(
+		&bcc, "bcc",
+		"b", false, "abre o site do bcc",
+	)
+
+	siteCmd.Flags().BoolVarP(
+		&neuro, "neuro",
+		"n", false, "abre o site da neuro",
 	)
 
 	rootCmd.AddCommand(siteCmd)
