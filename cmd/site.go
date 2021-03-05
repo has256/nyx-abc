1df95ff540551b12dc825befabe91c8d0bf8ff33
Carlos EA. Batista <carlos.batista@eiprice.com.br>
2021-03-04 22:09:00 -0300

    :hammer: refactor site

--- cmd/site.go
+++ cmd/site.go
@@ -14,96 +14,42 @@ func openBrowser(url string) {
 	if err != nil {
 		log.Fatal(err)
 	}
+	os.Exit(0)
 }
 
 var (
-	tidia   bool
-	prograd bool
-	gradmat bool
-	bcc     bool
-	sigaa   bool
-	moodle  bool
-	neuro   bool
-	ufabc   bool
 	siteCmd = &cobra.Command{
 		Use:   "site",
 		Short: "Abre os horrorosos sites da UFABC",
 		Run: func(cmd *cobra.Command, args []string) {
-			// refact, do we have PM? Switch/case?
-			if tidia {
-				openBrowser("https://tidia4.ufabc.edu.br/portal/relogin")
-				os.Exit(0)
-			} else if prograd {
-				openBrowser("https://prograd.ufabc.edu.br/")
-				os.Exit(0)
-			} else if gradmat {
-				openBrowser("https://gradmat.ufabc.edu.br/")
-				os.Exit(0)
-			} else if bcc {
-				openBrowser("https://bcc.ufabc.edu.br")
-				os.Exit(0)
-			} else if sigaa {
-				openBrowser("https://sig.ufabc.edu.br/sigaa/verTelaLogin.do")
-				os.Exit(0)
-			} else if moodle {
-				openBrowser("https://moodle.ufabc.edu.br/")
-				os.Exit(0)
-			} else if neuro {
-				openBrowser("https://neuro.ufabc.edu.br/")
-				os.Exit(0)
-			} else if ufabc {
-				openBrowser("https://www.ufabc.edu.br/")
-				os.Exit(0)
-			}
-
 			if len(args) == 0 {
 				cmd.Help()
+				os.Exit(-1)
 			}
+
+			switch args[0] {
+			case "tidia":
+				openBrowser("https://tidia4.ufabc.edu.br/portal/relogin")
+			case "prograd":
+				openBrowser("https://prograd.ufabc.edu.br/")
+			case "gradmat":
+				openBrowser("https://gradmat.ufabc.edu.br/")
+			case "bcc":
+				openBrowser("https://bcc.ufabc.edu.br")
+			case "sigaa":
+				openBrowser("https://sig.ufabc.edu.br/sigaa/verTelaLogin.do")
+			case "moodle":
+				openBrowser("https://moodle.ufabc.edu.br/")
+			case "neuro":
+				openBrowser("https://neuro.ufabc.edu.br/")
+			case "ufabc":
+				openBrowser("https://www.ufabc.edu.br/")
+			}
+
 		},
 	}
 )
 
 func init() {
-
-	siteCmd.Flags().BoolVarP(
-		&tidia, "tidia",
-		"t", false, "abre o site do tidia",
-	)
-
-	siteCmd.Flags().BoolVarP(
-		&prograd, "prograd",
-		"p", false, "abre o site da prograd",
-	)
-
-	siteCmd.Flags().BoolVarP(
-		&gradmat, "gradmat",
-		"g", false, "abre o site do gradmat",
-	)
-
-	siteCmd.Flags().BoolVarP(
-		&sigaa, "sigaa",
-		"s", false, "abre o site do sigaa",
-	)
-
-	siteCmd.Flags().BoolVarP(
-		&moodle, "moodle",
-		"m", false, "abre o site do moodle",
-	)
-
-	siteCmd.Flags().BoolVarP(
-		&ufabc, "ufabc",
-		"u", false, "abre o site da ufabc",
-	)
-
-	siteCmd.Flags().BoolVarP(
-		&bcc, "bcc",
-		"b", false, "abre o site do bcc",
-	)
-
-	siteCmd.Flags().BoolVarP(
-		&neuro, "neuro",
-		"n", false, "abre o site da neuro",
-	)
-
 	rootCmd.AddCommand(siteCmd)
 }
