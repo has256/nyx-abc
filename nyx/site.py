17e13cc72e2a61a9df98be1a8124e1834be5109e
Carlos EA. Batista <carlos.batista@eiprice.com.br>
2021-04-10 13:14:30 -0300

    refactor: add type hinting

--- nyx/site.py
+++ nyx/site.py
@@ -2,34 +2,39 @@ from enum import Enum
 import click
 
 
+Url = str
+SubCommand = click.core.Command
+CommandGroup = click.core.Command
+
+
 class WebsitesUFABC(Enum):
 
-    bcc = 'https://bcc.ufabc.edu.br'
-    gradmat = 'https://gradmat.ufabc.edu.br/'
-    moodle = 'https://moodle.ufabc.edu.br/'
-    neuro = 'https://neuro.ufabc.edu.br/'
-    prograd = 'https://prograd.ufabc.edu.br/'
-    sigaa = 'https://sig.ufabc.edu.br/sigaa/verTelaLogin.do'
-    tidia = 'https://tidia4.ufabc.edu.br/portal/relogin'
-    ufabc = 'https://www.ufabc.edu.br/'
+    bcc: Url = 'https://bcc.ufabc.edu.br'
+    gradmat: Url = 'https://gradmat.ufabc.edu.br/'
+    moodle: Url = 'https://moodle.ufabc.edu.br/'
+    neuro: Url = 'https://neuro.ufabc.edu.br/'
+    prograd: Url = 'https://prograd.ufabc.edu.br/'
+    sigaa: Url = 'https://sig.ufabc.edu.br/sigaa/verTelaLogin.do'
+    tidia: Url = 'https://tidia4.ufabc.edu.br/portal/relogin'
+    ufabc: Url = 'https://www.ufabc.edu.br/'
 
 
-def make_website_subcommand(name, url):
+def make_website_subcommand(name: str, url: Url) -> SubCommand:
 
-    fn_template = f'''@site.command()
+    fn_template: str = f'''@site.command()
 def {name}():
     """Abre o site do(a) {name}"""
     click.launch('{url}')
     '''
 
     exec(fn_template)
-    website_subcommand = locals()[f'{name}']
+    website_subcommand: SubCommand = locals()[f'{name}']
 
     return website_subcommand
 
 
 @ click.group('site')
-def site():
+def site() -> CommandGroup:
     """Utilitarios de Sites da UFABC"""
     ...
 
