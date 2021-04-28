32f1f7965ae7f763fe003d4580d2275eb217872b
= <phcfor@gmail.com>
2021-04-27 18:11:34 -0700

    nyx ementas --bibliografia

--- nyx/__init__.py
+++ nyx/__init__.py
@@ -2,6 +2,7 @@ from .disciplinas import disciplinas
 from .site import site
 from .aluno import aluno
 from .docente import docente
+from .ementas import ementas
 import click
 
 
@@ -14,5 +15,6 @@ cli.add_command(disciplinas)
 cli.add_command(site)
 cli.add_command(aluno)
 cli.add_command(docente)
+cli.add_command(ementas)
 
 cli()
