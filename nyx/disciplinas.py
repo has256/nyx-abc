b2caba2dd268cfc8d55fdf82003f8dd2eb6d82cd
Carlos EA. Batista <carlos.batista@eiprice.com.br>
2021-05-04 10:16:00 -0300

    feat: add preliminar watch command, need refactor asap - DRY and complexity issues

--- nyx/disciplinas.py
+++ nyx/disciplinas.py
@@ -2,6 +2,7 @@ import click
 import requests
 import pandas as pd
 import json
+import time
 
 
 @click.group('disciplinas')
@@ -66,3 +67,40 @@ def vazia():
             requisicoes = contagem_disciplinas[str(disciplina['id'])]
             if int(requisicoes) < (disciplina['vagas']):
                 print(disciplina['nome'])
+
+
+@disciplinas.command()
+def watch():
+    '''Acompanha o sistema de matriculas real-time'''
+    while True:
+        old_disciplinas = get_disciplina()
+        time.sleep(5)
+        new_disciplinas = get_disciplina()
+        if old_disciplinas == new_disciplinas:
+            print('Nothing Changed')
+        else:
+            print('Changed!!!')
+            for i, disciplina in enumerate(new_disciplinas):
+                if disciplina[1] != old_disciplinas[i][1]:
+                    print(f'{disciplina[0]} tem {disciplina[1]} vagas')
+
+
+def get_disciplina():
+    todas_disciplinas = requests.get(
+        'https://matricula.ufabc.edu.br/cache/todasDisciplinas.js')
+    disciplinas_ofertadas = todas_disciplinas.content.decode("utf-8")[17:-2]
+    disciplinas_ofertadas = json.loads(disciplinas_ofertadas)
+
+    contagem_disciplinas = requests.get(
+        'https://matricula.ufabc.edu.br/cache/contagemMatriculas.js')
+    contagem_disciplinas = contagem_disciplinas.content.decode("utf-8")[19:-2]
+    contagem_disciplinas = json.loads(contagem_disciplinas)
+
+    resultado = []
+
+    for disciplina in disciplinas_ofertadas:
+        if str(disciplina['id']) in contagem_disciplinas.keys():
+            resultado.append(
+                (disciplina['nome'], disciplina['vagas'] - int(contagem_disciplinas[str(disciplina['id'])])))
+
+    return resultado
--- nyx/ementas.py
+++ nyx/ementas.py
@@ -2,24 +2,23 @@ import click
 import pandas as pd
 import os
 
+
 @click.command('ementas')
 @click.argument('disciplina')
 @click.option('--bibliografia', is_flag=True)
 def ementas(disciplina, bibliografia):
     """Utilitarios de Ementas"""
-    ementas = pd.read_excel("https://github.com/EduRenesto/ufabc-manpages/blob/master/disciplinas.xlsx?raw=true")
-    query = ementas.query(f'DISCIPLINA.str.contains("{disciplina}")', engine = 'python')
-    click.clear()
+    ementas = pd.read_excel(
+        "https://github.com/EduRenesto/ufabc-manpages/blob/master/disciplinas.xlsx?raw=true")
+    query = ementas.query(
+        f'DISCIPLINA.str.contains("{disciplina}")', engine='python')
     for index, row in query.iterrows():
         click.secho(row.DISCIPLINA + ":", fg='blue')
-        click.secho(f"TPI: {row.TPI}, Recomendação: {row.RECOMENDAÇÃO}", fg='red', blink=True)
+        click.secho(
+            f"TPI: {row.TPI}, Recomendação: {row.RECOMENDAÇÃO}", fg='red')
         if bibliografia:
             click.secho("Bibliografia Sugerida:", fg='green', underline=True)
             click.secho(f"{row['BIBLIOGRAFIA BÁSICA']}\n", fg='green')
         click.secho("Ementa:", fg='green', underline=True)
         click.secho(f"{row.EMENTA}\n", fg='green')
         click.secho(f"-"*78, fg='yellow')
-
-
-
-
