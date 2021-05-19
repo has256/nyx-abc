b15f70aa905b9e1525fcd217a50cf624bbe834c2
Carlos EA. Batista <carlos.batista@eiprice.com.br>
2021-05-19 19:16:55 -0300

    dist: minor version that works, module name...

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='nyx-abc',
-    version='0.1.0',
+    version='0.1.1',
     license='MIT',
     author='Carlos EA. Batista',
     author_email='cardu.chmod.777@gmail.com',
